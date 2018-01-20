b6b486f7fcb8c30bc6ec0624a1082682370a1089
Samahara <le.chat.au.chapeau@hotmail.com>
2018-01-19 21:16:26 -0600

    retos de codigo

---
+++ js/data.js
@@ -0,0 +1,19 @@
+var data = {
+    'restaurantes' : [
+        {
+            'adress' : 'Acueducto #5098 Xochimilco, CDMX',
+            'name' : 'El Rinconcito',
+            'food' : 'Mexicana',
+            'photo' : 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+            'service' : true,
+        },
+        {
+            'adress' : 'Halcon #4059 Xochimilco, CDMX',
+            'name' : 'La Chilanguita',
+            'food' : 'Mexicana',
+            'photo' : 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+            'service' : false,
+        }
+    ]
+};
+console.log(data);
