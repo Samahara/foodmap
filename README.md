97d08dd1d3b0be418146545044ece80c5838f225
Samahara <le.chat.au.chapeau@hotmail.com>
2018-01-21 20:02:50 -0600

    accediendo a la data

---
+++ README.md
@@ -0,0 +1,19 @@
+# Foodmap
+
+## Objetivo
+
+1. Vista splash con duración de 2 a 5 segundos.
+![Vista splash](assets/images/splash.jpg)
+
+2. En la vista principal se muestran todos los restaurantes "cerca de ti" junto con el input para el filtro.
+En esta ocasión lo haremos por tipo de comida y servicio a domicilio.
+![Página principal](assets/images/2.jpg)
+
+3. Ya que se hizo la selección de filtro debe mostrarse únicamente aquellos restaurantes que cumplan la condición.
+![Filtro](assets/images/3.jpg)
+
+4. Al seleccionar alguno de los restaurantes, deberá mostrarse la información de éste a través de un modal.
+![Modal](assets/images/5.jpg)
+
+5. Una vez cerrado ese modal debe volver a la vista principal.
+![Página principal](assets/images/2.jpg)
--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 /*Variables reutilizables*/
 var $input = $('#icon_prefix');
-
+var $restaurantName = $('.restaurantName');
+var $especialFood = $('.especialFood');
+var $adressRestaurant = $('.adressRestaurant');
+var $serviceButton = $('.service');
+var $imageFood = $('.food-photo');
+var $order = $('.buttonOrder');
 
 function loadPage() {
   $('#modal1').modal();
+  $imageFood.click(selectImg);
+}
+
+function selectImg() {
+  var $restaurant = event.target.dataset.name;
+  var $dataRestaurant = data[$restaurant];
+
+  var $name = $dataRestaurant['name'];
+  var $adress = $dataRestaurant['adress'];
+  var $special = $dataRestaurant['special-food'];
+  var $service = $dataRestaurant['service'];
+
+  infoModal($name,$adress,$special,$service);
+}
+
+function infoModal($name,$adress,$special,$service) {
+  $restaurantName.text("");
+  $especialFood.text("");
+  $adressRestaurant.text("");
+
+  $restaurantName.text($name);
+  $especialFood.text($special);
+  $adressRestaurant.text($adress);
+
 }
 
 $(document).ready(loadPage);
